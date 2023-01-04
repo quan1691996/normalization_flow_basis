819841a49adaa11a1f2fc50a071be33053c94ff6
Luong Do Anh Quan <luongdoanhquan1996@gmail.com>
2023-01-04 23:19:03 +0900

    3. Flow2d

--- utils.py
+++ utils.py
@@ -67,6 +67,46 @@ class FlowComposable1d(nn.Module):
             sum_log_dz_by_dx += log_dz_by_dx
         return z, sum_log_dz_by_dx
 
+class CDFParams(nn.Module):
+    def __init__(self, input_size=1, hidden_size=64, num_hidden_layers=3, output_size=None):
+        super(CDFParams, self).__init__()
+        layers = [nn.Linear(input_size, hidden_size), nn.ReLU()]
+        for _ in range(num_hidden_layers - 1):
+            layers.append( nn.Linear(hidden_size, hidden_size) )
+            layers.append( nn.ReLU() )
+        layers.append( nn.Linear(hidden_size, output_size) )
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+
+class ConditionalFlow1D(nn.Module):
+    def __init__(self, n_components):
+        super(ConditionalFlow1D, self).__init__()
+        self.cdf = CDFParams(output_size=n_components*3)
+
+    def forward(self, x, condition):
+        x = x.view(-1,1)
+        mus, log_sigmas, weight_logits = torch.chunk(self.cdf(condition), 3, dim=1)
+        weights = weight_logits.softmax(dim=1)
+        distribution = Normal(mus, log_sigmas.exp())
+        z = (distribution.cdf(x) * weights).sum(dim=1)
+        log_dz_by_dx = (distribution.log_prob(x).exp() * weights).sum(dim=1).log()
+        return z, log_dz_by_dx
+
+class Flow2d(nn.Module):
+    def __init__(self, n_components):
+        super(Flow2d, self).__init__()
+        self.flow_dim1 = Flow1d(n_components)
+        self.flow_dim2 = ConditionalFlow1D(n_components)
+
+    def forward(self, x):
+        x1, x2 = torch.chunk(x, 2, dim=1)
+        z1, log_dz1_by_dx1 = self.flow_dim1(x1)
+        z2, log_dz2_by_dx2 = self.flow_dim2(x2, condition=x1)
+        z = torch.cat([z1.unsqueeze(1), z2.unsqueeze(1)], dim=1)
+        log_dz_by_dx = torch.cat([log_dz1_by_dx1.unsqueeze(1), log_dz2_by_dx2.unsqueeze(1)], dim=1)
+        return z, log_dz_by_dx
 
 def loss_function(target_distribution, z, log_dz_by_dx):
     log_likelihood = target_distribution.log_prob(z) + log_dz_by_dx
@@ -75,6 +115,7 @@ def loss_function(target_distribution, z, log_dz_by_dx):
 def train(model, train_loader, optimizer, target_distribution):
     model.train()
     for x in train_loader:
+        x = x.float()
         z, log_dz_by_dx = model(x)
         loss = loss_function(target_distribution, z, log_dz_by_dx)
         optimizer.zero_grad()
@@ -85,6 +126,7 @@ def eval_loss(model, data_loader, target_distribution):
     model.eval()
     total_loss = 0
     for x in data_loader:
+        x = x.float()
         z, log_dz_by_dx = model(x)
         loss = loss_function(target_distribution, z, log_dz_by_dx)
         total_loss += loss * x.size(0)
